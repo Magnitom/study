d09821da3ad490df72965718f1d3693773328f5c
John Doe <sterhovpp@yandex.ru>
2022-05-23 22:14:00 +0300

    modify syntaxNumber2Task1 by refactor

--- src/main/java/ru/magnitom/syntaxNumber2Task1.java
+++ src/main/java/ru/magnitom/syntaxNumber2Task1.java
@@ -7,13 +7,13 @@ public class syntaxNumber2Task1 {
         Scanner input = new Scanner(System.in);
         String stringInput = input.nextLine();
         if (stringInput.length() < 5) {
-            if (stringInput.substring(0, 1).equals("а")) {
+            if (stringInput.charAt(0) == 'а') {
                 System.out.println("Первая буква - а");
             } else {
-                System.out.println(stringInput.substring(1, stringInput.length()));
+                System.out.println(stringInput.substring(1));
             }
         } else if (stringInput.length() > 5) {
-            if (stringInput.substring(stringInput.length() - 1, stringInput.length()).equals("я")) {
+            if (stringInput.charAt(stringInput.length() - 1) == 'я') {
                 System.out.println("Последняя буква - я");
             } else {
                 System.out.println(stringInput.substring(0, stringInput.length() - 1));
