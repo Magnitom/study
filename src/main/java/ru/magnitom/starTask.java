6190dd359b0c70717fb566d008289d4b57345571
Magnitom <sterhovpp@gmail.com>
2022-05-24 19:48:39 +0300

    upgrade task star

--- src/main/java/ru/magnitom/starTask.java
+++ src/main/java/ru/magnitom/starTask.java
@@ -6,14 +6,16 @@ public class starTask {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numberInput = sc.nextInt();
-        String star = "*";
-        for (int i = 0; i < numberInput-1; i++) {
-            star = star + "*";
+        StringBuilder star = new StringBuilder("*");
+        for (int i = 0; i < numberInput - 1; i++) {
+            star.append("*");
         }
         for (int i = 0; i < numberInput; i++) {
-            System.out.println();
+            if (i != 0) {
+                System.out.println();
+            }
             System.out.print(star);
-            star = star.substring(0, star.length()-1);
+            star = new StringBuilder(star.substring(0, star.length() - 1));
         }
     }
 }
