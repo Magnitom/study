1bf5f3d9be208c66649a65bc913aaf98bb39b269
Magnitom <sterhovpp@gmail.com>
2022-05-26 21:22:40 +0300

    modify VaragsItPark

--- src/main/java/ru/magnitom/VaragsItPark.java
+++ src/main/java/ru/magnitom/VaragsItPark.java
@@ -1,26 +1,27 @@
-//package ru.magnitom;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class VaragsItPark {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        String stringInput = sc.nextLine();
-//        List<String> listString = new ArrayList<>();
-//        while (sc.hasNext()){
-//            listString.add(sc.nextLine());
-//        }
-//        System.out.println(CompSt(stringInput, listString));
-//    }
-//
-//    public static boolean CompSt (String stringIn, String... arr){
-//        String resultArr = arr[0];
-//        for (int i = 0; i < arr.length; i++) {
-//            resultArr = arr[i] + resultArr;
-//        }
-//        boolean result = stringIn.equals(resultArr);
-//        return result;
-//    }
-//}
+package ru.magnitom;
+
+import java.util.Scanner;
+
+public class VaragsItPark {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ввыелите строку");
+        String stringInput = sc.nextLine();
+        System.out.println("Введите количество передаваемых символов");
+        int lenghtArr = sc.nextInt();
+        String[] arrString = new String[lenghtArr];
+        sc.nextLine();
+        for (int i = 0; i < arrString.length; i++) {
+            arrString[i] = sc.nextLine();
+        }
+        System.out.println(CompSt(stringInput, arrString));
+    }
+
+    public static boolean CompSt(String stringIn, String... arr) {
+        String resultArr = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            resultArr = resultArr + arr[i];
+        }
+        return stringIn.equals(resultArr);
+    }
+}
